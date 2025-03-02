44f7260fc5b63056d3a4903fd89ee096c70d66bb
Klaus Reimer <k@ailis.de>
2025-03-02 21:32:29 +0100

    Add Name_Localised property

--- src/main/events/odyssey/DropItems.ts
+++ src/main/events/odyssey/DropItems.ts
@@ -7,6 +7,7 @@ import type { JournalEvent } from "../../JournalEvent.js";
 
 export interface DropItems extends JournalEvent<"DropItems"> {
     Name: string;
+    Name_Localised?: string;
     Type: string;
     OwnerID: number;
     Count: number;
