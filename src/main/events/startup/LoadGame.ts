435675ca0cee683c689ed2610664311d5f63ca57
Klaus Reimer <k@ailis.de>
2025-08-24 11:08:58 +0200

    Improve documentation

--- src/main/events/startup/LoadGame.ts
+++ src/main/events/startup/LoadGame.ts
@@ -16,22 +16,22 @@ export interface LoadGame extends JournalEvent<"LoadGame"> {
     /** The commander's name. */
     Commander: string;
 
-    /** The player ID. */
+    /** The player ID. Not set in older journals (before December 2018). */
     FID?: string;
 
-    /** True when Horizons. */
+    /** True when game has Horizons. Not set before Horizons. */
     Horizons?: boolean;
 
-    /** True when Odyssey. */
+    /** True when game has Odyssey. Not set before Odyssey. */
     Odyssey?: boolean;
 
-    /** The current ship type. */
+    /** The current ship type. Can be missing for unknown reasons, maybe a game bug. */
     Ship?: string;
 
-    /** The ID of the current ship. */
+    /** The ID of the current ship. Not set if player is currently in an Apex Shuttle. */
     ShipID?: ID;
 
-    /** Localised ship type. */
+    /** Localised ship type. Not always set unfortunately, so a custom translation from {@link Ship} value is needed. */
     Ship_Localised?: string;
 
     /** True if landed. */
@@ -40,10 +40,10 @@ export interface LoadGame extends JournalEvent<"LoadGame"> {
     /** True if starting dead. See {@link Resurrect}. */
     StartDead?: boolean;
 
-    /** The game mode. */
+    /** The game mode. Can be missing for unknown reasons, maybe a game bug. */
     GameMode?: GameMode;
 
-    /** Name of the group (if in a group. */
+    /** Name of the group (if in a group). Not set if game mode is Open or Solo. */
     Group?: string;
 
     /** Current credit balance. */
@@ -52,19 +52,24 @@ export interface LoadGame extends JournalEvent<"LoadGame"> {
     /** Current loan. */
     Loan: number;
 
-    /** User-defined ship name. */
+    /** User-defined ship name. Not set in journals from older game versions or when currently flying in an Apex Shuttle. */
     ShipName?: string;
 
-    /** User-defines ship ID. */
+    /** User-defined ship ID. Not set in journals from older game versions or when currently flying in an Apex Shuttle. */
     ShipIdent?: string;
 
-    /** Current fuel level. */
+    /** Current fuel level. Not set in journals from older game versions or when currently flying in an Apex Shuttle. */
     FuelLevel?: number;
 
-    /** Size if main tank. */
+    /** Size if main tank. Not set in journals from older game versions or when currently flying in an Apex Shuttle. */
     FuelCapacity?: number;
 
+    /** The game language. Set since Odyssey Update 5 (July 2021). */
     language?: string;
+
+    /** The game version. Set since Odyssey Update 5 (July 2021). */
     gameversion?: string;
+
+    /** The game build. Set since Odyssey Update 5 (July 2021). */
     build?: string;
 }
